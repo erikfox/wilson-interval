ad7982acb4c1161e1224269051e10c359c8cd237
Erik Fox <erik@erikfox.co>
2017-08-03 18:32:54 -0400

    Remove continuity correction

--- src/index.js
+++ src/index.js
@@ -1,75 +1,29 @@
 import math from 'mathjs';
 import pnormaldist from 'pnormaldist';
 
-export function isNonNegativeInteger(input) {
-  if (typeof input !== 'number') return false;
-  if (isNaN(input)) return false;
-  if (Math.sign(input) === -1) return false;
-  if (!math.isInteger(input)) return false;
-  return true;
-}
-
-export function isValidOptions(options) {
-  if (typeof options !== 'object') return false;
-  return true;
-}
-
-export function getPopulationScaleFactor(sample, population) {
-  return math.eval(
-    'sqrt(1 - sample / population)',
-    { sample, population },
-  );
-}
-
-/*
- * credit: Sean Wallis, Survey of English Usage, University College of London
- * source: https://corplingstats.wordpress.com/2012/04/30/inferential-statistics/
- */
-export default function (observed, sample, options = {}) {
-  if (!isNonNegativeInteger(observed)) throw new Error('Argument "observed" must be a non-negative whole number');
-  if (!isNonNegativeInteger(sample)) throw new Error('Argument "sample" must be a non-negative whole number');
-  if (observed > sample) throw new Error('Argument "observed" cannot be greater than "sample"');
-
-  // if (options && !isValidOptions(options)) throw new Error('Options not valid');
-
+export default function wilson(observed, sample, population = false, options = {}) {
   const {
-    precision = 20,
-    continuity = false,
-  } = options;
-
-  math.config({
-    number: 'BigNumber',
-    precision,
-  });
-
-  let {
     confidence = 0.95,
-    population = false,
+    precision = 20,
   } = options;
+
+  math.config({ number: 'BigNumber', precision });
 
   observed = math.bignumber(observed);
   sample = math.bignumber(sample);
-  confidence = math.bignumber(confidence);
-  population = population ? math.bignmbuer(population) : population;
 
-  // proportion of positive outcomes
-  const proportion = math.eval(
-    'observed / sample',
-    { observed, sample },
-  );
+  const proportion = math.eval('observed / sample', { observed, sample });
 
-  // if population size given
   if (population) {
-    sample /= getPopulationScaleFactor(sample, population);
+    sample = math.eval('sample / (sqrt(1 - sample / population))', { sample, population });
   }
 
-  // calculate z-score: http://www.evanmiller.org/how-not-to-sort-by-average-rating.html
-  const zScore = pnormaldist(1 - (1 - confidence) / 2);
+  const zScore = pnormaldist(math.eval('1 - (1 - confidence) / 2', { confidence }));
 
-  return continuity ? wilsonContinuity(proportion, sample, zScore) : wilsonStandard(proportion, sample, zScore);
+  return wilsonInterval(proportion, sample, zScore);
 }
 
-function wilsonStandard(proportion, sample, zScore) {
+function wilsonInterval(proportion, sample, zScore) {
   const scope = {
     p: math.bignumber(proportion),
     n: math.bignumber(sample),
@@ -88,35 +42,6 @@ function wilsonStandard(proportion, sample, zScore) {
 
   const center = math.format(math.eval(
     '(p + z * z / (2 * n)) / (1 + z * z / n)',
-    scope,
-  ));
-
-  return {
-    high,
-    center,
-    low,
-  };
-}
-
-function wilsonContinuity(proportion, sample, zScore) {
-  const scope = {
-    p: math.bignumber(proportion),
-    n: math.bignumber(sample),
-    z: math.bignumber(zScore),
-  };
-
-  const high = math.format(math.eval(
-    '(2 * n * p + z * z + (z * sqrt(z * z - 1 / n + 4 * n * p * (1 - p) - (4 * p - 2)) + 1)) / (2 * (n + z * z))',
-    scope,
-  ));
-
-  const low = math.format(math.eval(
-    '(2 * n * p + z * z - (z * sqrt(z * z - 1 / n + 4 * n * p * (1 - p) + (4 * p - 2)) + 1)) / (2 * (n + z * z))',
-    scope,
-  ));
-
-  const center = math.format(math.eval(
-    '(2 * n * p + z * z) / (2 * (n + z * z))',
     scope,
   ));
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,116 +1,27 @@
-import wilson, {
-  isNonNegativeInteger,
-  isValidOptions,
-  getPopulationScaleFactor,
-} from '.';
-
-describe('getPopulationScaleFactor()', () => {
-  test('getPopulationScaleFactor(100, 227)', () => {
-    expect(getPopulationScaleFactor(100, 227)).toMatchSnapshot();
-  });
-});
-
-describe('isNonNegativeInteger()', () => {
-  describe('returns false', () => {
-    test('undefined', () => {
-      expect(isNonNegativeInteger(undefined)).toEqual(false);
-    });
-
-    test('false', () => {
-      expect(isNonNegativeInteger(false)).toEqual(false);
-    });
-
-    test('null', () => {
-      expect(isNonNegativeInteger(null)).toEqual(false);
-    });
-
-    test('""', () => {
-      expect(isNonNegativeInteger('')).toEqual(false);
-    });
-
-    test('"1"', () => {
-      expect(isNonNegativeInteger('1')).toEqual(false);
-    });
-
-    test('-1', () => {
-      expect(isNonNegativeInteger(-1)).toEqual(false);
-    });
-
-    test('1.0', () => {
-      expect(isNonNegativeInteger(1.1)).toEqual(false);
-    });
-  });
-
-  describe('returns true', () => {
-    test('0', () => {
-      expect(isNonNegativeInteger(0)).toEqual(true);
-    });
-
-    test('1', () => {
-      expect(isNonNegativeInteger(1)).toEqual(true);
-    });
-  });
-});
-
-describe('isValidOptions()', () => {
-  describe('returns false', () => {
-    describe('when isValidOptions(options)', () => {
-      test('false', () => {
-        expect(isValidOptions(false)).toEqual(false);
-      });
-    });
-  });
-
-  describe('returns true', () => {
-    test('{ continuity: true }', () => {
-      expect(isValidOptions({ continuity: true })).toEqual(true);
-    });
-  });
-});
+import wilson from '.';
 
 describe('wilson()', () => {
-  describe('throws Error', () => {
-    describe('when wilson(observed, ...) is invalid', () => {
-      test('= undefined', () => {
-        expect(() => wilson(undefined, 100))
-          .toThrowErrorMatchingSnapshot();
-      });
-
-      test('greater than wilson(..., sample)', () => {
-        expect(() => wilson(2, 1))
-          .toThrowErrorMatchingSnapshot();
-      });
+  describe('returns accurate results', () => {
+    test('wilson(5, 100)', () => {
+      const result = wilson(5, 100);
+      expect(result).toMatchSnapshot();
     });
 
-    describe('when wilson(..., sample) is invalid', () => {
-      test('= undefined', () => {
-        expect(() => wilson(1, undefined))
-          .toThrowErrorMatchingSnapshot();
-      });
+    test('wilson(5, 100, 227)', () => {
+      const result = wilson(5, 100, 227);
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  describe('accepts options', () => {
+    test('wilson(5, 100, false, { precision: 6 })', () => {
+      const result = wilson(5, 100, false, { precision: 6 });
+      expect(result).toMatchSnapshot();
     });
 
-    describe('when wilson(..., options) is invalid', () => {}); // TODO
-  });
-});
-
-describe('Accurate results', () => {
-  test('wilson(1, 1)', () => {
-    const result = wilson(1, 1);
-    expect(result).toMatchSnapshot();
-  });
-
-  test('wilson(40, 100)', () => {
-    const result = wilson(40, 100);
-    expect(result).toMatchSnapshot();
-  });
-
-  test('wilson(40, 100, { continuity: true })', () => {
-    const result = wilson(40, 100, { continuity: true });
-    expect(result).toMatchSnapshot();
-  });
-
-  test('wilson(1, 1, { continuity: true })', () => {
-    const result = wilson(1, 1, { continuity: true });
-    expect(result).toMatchSnapshot();
+    test('wilson(5, 100, 227, { confidence: 0.9 })', () => {
+      const result = wilson(5, 100, 227, { confidence: 0.9 });
+      expect(result).toMatchSnapshot();
+    });
   });
 });
