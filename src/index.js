1c3955b330c9d0c0b82dd87ff1dd630689d0e149
Erik Fox <erik@erikfox.co>
2017-08-09 17:31:03 -0400

    Add flow typings and prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+// @flow
 import math from 'mathjs';
 import pnormaldist from 'pnormaldist';
 
-export default function wilson(observed, sample, population = false, options = {}) {
-  const {
-    confidence = 0.95,
-    precision = 20,
-  } = options;
+export default function wilson(
+  observed: number,
+  sample: number,
+  population: ?boolean = false,
+  options: { confidence?: number, precision?: number } = {}
+) {
+  const { confidence = 0.95, precision = 20 } = options;
 
   math.config({ number: 'BigNumber', precision });
 
@@ -15,35 +18,43 @@ export default function wilson(observed, sample, population = false, options = {
   const proportion = math.eval('observed / sample', { observed, sample });
 
   if (population) {
-    sample = math.eval('sample / (sqrt(1 - sample / population))', { sample, population });
+    sample = math.eval('sample / (sqrt(1 - sample / population))', {
+      sample,
+      population,
+    });
   }
 
-  const zScore = pnormaldist(math.eval('1 - (1 - confidence) / 2', { confidence }));
+  const zScore = pnormaldist(
+    math.eval('1 - (1 - confidence) / 2', { confidence })
+  );
 
   return wilsonInterval(proportion, sample, zScore);
 }
 
-function wilsonInterval(proportion, sample, zScore) {
+function wilsonInterval(proportion: number, sample: number, zScore: number) {
   const scope = {
     p: math.bignumber(proportion),
     n: math.bignumber(sample),
     z: math.bignumber(zScore),
   };
 
-  const high = math.format(math.eval(
-    '(p + z * z / (2 * n) + z * sqrt(p * (1 - p) / n + z * z / (4 * (n * n)))) / (1 + z * z / n)',
-    scope,
-  ));
+  const high = math.format(
+    math.eval(
+      '(p + z * z / (2 * n) + z * sqrt(p * (1 - p) / n + z * z / (4 * (n * n)))) / (1 + z * z / n)',
+      scope
+    )
+  );
 
-  const low = math.format(math.eval(
-    '(p + z * z / (2 * n) - z * sqrt(p * (1 - p) / n + z * z / (4 * (n * n)))) / (1 + z * z / n)',
-    scope,
-  ));
+  const low = math.format(
+    math.eval(
+      '(p + z * z / (2 * n) - z * sqrt(p * (1 - p) / n + z * z / (4 * (n * n)))) / (1 + z * z / n)',
+      scope
+    )
+  );
 
-  const center = math.format(math.eval(
-    '(p + z * z / (2 * n)) / (1 + z * z / n)',
-    scope,
-  ));
+  const center = math.format(
+    math.eval('(p + z * z / (2 * n)) / (1 + z * z / n)', scope)
+  );
 
   return {
     high,
